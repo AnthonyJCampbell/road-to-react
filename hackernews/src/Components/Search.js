08abe2c25716e97f71d0acec7ef837db2178260b
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-04-02 15:53:05 +0200

    Added focus on Search Component on first render

--- hackernews/src/Components/Search.js
+++ hackernews/src/Components/Search.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from './Button';
 import PropTypes from 'prop-types';
 
-const Search = ({ 
-    value, 
-    onChange, 
-    children,
-    onSubmit
-  }) => {
-  return (
-    <>
-      <form onSubmit={onSubmit}>
-          {children} <input
-            type="text"
-            value={value}
-            onChange={onChange}
-          />
-          <Button type="submit" onClick={onSubmit}>
-            {children}
-          </Button>
-      </form>
-    </>
-  );
+class Search extends Component {
+  componentDidMount() {
+    if(this.input) {
+      this.input.focus()
+    }
+  }
+  render() {
+    const { 
+      value, 
+      onChange, 
+      children,
+      onSubmit
+    } = this.props;
+    return (
+      <>
+        <form onSubmit={onSubmit}>
+            {children} <input
+              type="text"
+              value={value}
+              onChange={onChange}
+              ref={(node) => {this.input = node;}}
+            />
+            <Button type="submit" onClick={onSubmit}>
+              {children}
+            </Button>
+        </form>
+      </>
+    )
+  }
 }
 
 Search.propTypes = {
