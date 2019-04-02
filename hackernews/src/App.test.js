cfe8fd5a8499d6bbe321499de2b4ba836322d4d3
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-04-02 15:04:14 +0200

    Added Enzyme for testing

--- hackernews/src/App.test.js
+++ hackernews/src/App.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer'
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// Components
 import App from './App';
 import Search from './Components/Search';
 import Button from './Components/Button';
 import Table from './Components/Table'
-import renderer from 'react-test-renderer'
+
+// Initialization of Enzyme
+Enzyme.configure({ adapter: new Adapter()});
+
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -65,5 +73,10 @@ describe('Table', () => {
     const component = renderer.create(<Table {...props}/>)
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot()
+  })
+
+  it('shows two items in list', () => {
+    const element = shallow(<Table {...props}/>)
+    expect(element.find('.table-row').length).toBe(2)
   })
 })
